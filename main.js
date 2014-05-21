df292125ce577d45a437176bd98c1549e1f2f595
Kevin Martensson <kevinmartensson@gmail.com>
2014-05-21 10:48:53 +0200

    Major rewrite
    * Now using atom-shell instead of node-webkit (fixes #7)
    * Summary show correct results when optimizing multiple times (fixes #10)

--- main.js
+++ main.js
@@ -5,17 +5,11 @@ var BrowserWindow = require('browser-window');
 var crashReporter = require('crash-reporter');
 
 /**
- * Report crashes to our server
- */
-
-crashReporter.start();
-
-/**
  * Keep a global reference of the window object, if you don't, the window will
  * be closed automatically when the javascript object is GCed
  */
 
-var mainWindow = null;
+var win = null;
 
 /**
  * Quit when all windows are closed
@@ -28,13 +22,28 @@ app.on('window-all-closed', function () {
 });
 
 /**
+ * Report crashes to our server
+ */
+
+app.on('will-finish-launching', function () {
+    crashReporter.start({ productName: 'imagemin-app' });
+});
+
+/**
+ * Set badge text
+ */
+
+app.dock.setBadge('imagemin-app');
+
+/**
  * On ready
  */
 
-app.on('ready', function () {
-    mainWindow = new BrowserWindow({ width: 400, height: 400 });
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
-    mainWindow.on('closed', function () {
-        mainWindow = null;
+app.on('finish-launching', function () {
+    win = new BrowserWindow({ width: 500, height: 450 });
+    win.loadUrl('file://' + __dirname + '/index.html');
+
+    win.on('closed', function () {
+        win = null;
     });
 });
