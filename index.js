2ec57374d1eeca58728169b5d88f19b37226b5b5
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-19 18:47:11 +0200

    Add spinner

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var each = node('each-async');
 var fs = node('fs');
 var Imagemin = node('imagemin');
 var path = node('path');
+var Spinner = require('component/spinner');
 
 /**
  * Read file
@@ -16,14 +17,14 @@ var path = node('path');
  */
 
 function read(path, cb) {
-	fs.readFile(path, function (err, buf) {
-		if (err) {
-			cb(err);
-			return;
-		}
+    fs.readFile(path, function (err, buf) {
+        if (err) {
+            cb(err);
+            return;
+        }
 
-		cb(null, buf);
-	});
+        cb(null, buf);
+    });
 }
 
 /**
@@ -35,30 +36,73 @@ function read(path, cb) {
  */
 
 function minify(file, cb) {
-	read(file.path, function (err, buf) {
-		if (err) {
-			cb(err);
-			return;
-		}
+    read(file.path, function (err, buf) {
+        if (err) {
+            cb(err);
+            return;
+        }
 
-		var imagemin = new Imagemin()
-			.src(buf)
-			.dest(path.join(path.dirname(file.path), 'build', path.basename(file.path)))
-			.use(Imagemin.gifsicle())
-			.use(Imagemin.jpegtran())
-			.use(Imagemin.optipng())
-			.use(Imagemin.pngquant())
-			.use(Imagemin.svgo());
+        var imagemin = new Imagemin()
+            .src(buf)
+            .dest(path.join(path.dirname(file.path), 'build', path.basename(file.path)))
+            .use(Imagemin.gifsicle())
+            .use(Imagemin.jpegtran())
+            .use(Imagemin.optipng())
+            .use(Imagemin.pngquant())
+            .use(Imagemin.svgo());
 
-		imagemin.optimize(function (err, file) {
-			if (err) {
-				cb(err);
-				return;
-			}
+        imagemin.optimize(function (err, file) {
+            if (err) {
+                cb(err);
+                return;
+            }
 
-			cb(null, file);
-		});
-	});
+            cb(null, file);
+        });
+    });
+}
+
+/**
+ * Create spinner
+ *
+ * @api private
+ */
+
+function spin() {
+    var w = document.body.offsetWidth;
+    var h = document.body.offsetHeight;
+    var s = new Spinner()
+        .size(w / 4)
+        .light();
+
+    s.el.style.position = 'absolute';
+    s.el.style.top = h / 2 - (w / 4) / 2 + 'px';
+    s.el.style.left = w / 2 - (w / 4) / 2 + 'px';
+
+    spin.remove = function () {
+        document.body.removeChild(s.el);
+    };
+
+    document.body.appendChild(s.el);
+    return s;
+}
+
+/**
+ * Toggle display
+ *
+ * @param {Element} el
+ * @api private
+ */
+
+function toggle(el) {
+    el = document.querySelector(el);
+
+    if (el.style.display === 'none') {
+        el.style.display = 'block';
+        return;
+    }
+
+    el.style.display = 'none';
 }
 
 /**
@@ -66,24 +110,30 @@ function minify(file, cb) {
  */
 
 drop(function (e) {
-	var ret = [];
+    var ret = [];
 
-	each(e.items, function (item, i, done) {
-		minify(item, function (err, file) {
-			if (err) {
-				done(err);
-				return;
-			}
+    toggle('#drop-anywhere');
+    spin();
 
-			ret.push(file);
-			done();
-		});
-	}, function (err) {
-		if (err) {
-			console.error(err);
-			return;
-		}
+    each(e.items, function (item, i, done) {
+        minify(item, function (err, file) {
+            if (err) {
+                done(err);
+                return;
+            }
 
-		ret = [];
-	});
+            ret.push(file);
+            done();
+        });
+    }, function (err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        spin.remove();
+        toggle('#drop-anywhere');
+
+        ret = [];
+    });
 });
